70189090be82d23d8e549285309c49c2980be672
Denis Arh <denis.arh@gmail.com>
2022-03-31 20:27:21 +0200

    WIP

---
+++ store/adapters/rdbms/drivers/sqlserver/connect.go
@@ -0,0 +1,93 @@
+package sqlserver
+
+import (
+	"context"
+	"database/sql"
+	"net/url"
+	"strings"
+
+	"github.com/cortezaproject/corteza-server/store"
+	"github.com/cortezaproject/corteza-server/store/adapters/rdbms"
+	"github.com/cortezaproject/corteza-server/store/adapters/rdbms/instrumentation"
+	"github.com/davecgh/go-spew/spew"
+	_ "github.com/denisenkom/go-mssqldb"
+	mssql "github.com/denisenkom/go-mssqldb"
+	"github.com/doug-martin/goqu/v9"
+	_ "github.com/doug-martin/goqu/v9/dialect/sqlserver"
+	"github.com/ngrok/sqlmw"
+)
+
+func init() {
+	store.Register(Connect, "sqlserver", "sqlserver+debug")
+	sql.Register("sqlserver+debug", sqlmw.Driver(new(mssql.Driver), instrumentation.Debug()))
+}
+
+func Connect(ctx context.Context, dsn string) (_ store.Storer, err error) {
+	var (
+		cfg *rdbms.Config
+		s   *rdbms.Store
+	)
+
+	if cfg, err = ProcDataSourceName(dsn); err != nil {
+		return
+	}
+
+	cfg.ErrorHandler = errorHandler
+
+	if s, err = rdbms.Connect(ctx, cfg); err != nil {
+		return
+	}
+
+	cfg.Upgrader = NewUpgrader(s)
+	return s, nil
+}
+
+//func (s *Store) Upgrade(ctx context.Context, log *zap.Logger) (err error) {
+//	if err = (&rdbms.Schema{}).Upgrade(ctx, NewUpgrader(log, s)); err != nil {
+//		return fmt.Errorf("cannot upgrade sqlserver schema: %w", err)
+//	}
+//
+//	return nil
+//}
+
+// ProcDataSourceName validates given DSN and ensures
+// params are present and correct
+func ProcDataSourceName(dsn string) (c *rdbms.Config, err error) {
+	const (
+		validScheme = "sqlserver"
+	)
+	var (
+		scheme string
+		u      *url.URL
+	)
+
+	if u, err = url.Parse(dsn); err != nil {
+		return nil, err
+	}
+
+	if strings.HasPrefix(dsn, "sqlserver") {
+		scheme = u.Scheme
+		u.Scheme = validScheme
+	}
+
+	return &rdbms.Config{
+		DriverName:     scheme,
+		DataSourceName: u.String(),
+		DBName:         strings.Trim(u.Path, "/"),
+		Dialect:        goqu.Dialect("sqlserver"),
+	}, nil
+}
+
+func errorHandler(err error) error {
+	if err != nil {
+		spew.Dump(err)
+		//if implErr, ok := err.(*pq.Error); ok {
+		//	switch implErr.Code.Name() {
+		//	case "unique_violation":
+		//		return store.ErrNotUnique.Wrap(implErr)
+		//	}
+		//}
+	}
+
+	return err
+}
